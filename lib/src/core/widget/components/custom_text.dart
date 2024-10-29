f8b134c5911814c790d68cd4a75384430b5e5a10
dadikas <doanvannghi2002@gmail.com>
2024-10-29 08:33:19 +0700

    add components/customtext

---
+++ lib/src/core/widget/components/custom_text.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'package:libary_management/src/core/widget/utils/text_util.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final bool isBold;
+  final bool isItalic;
+  final bool isCenter;
+  final double fontSize;
+  final bool isOverFlow;
+  final Color? textColor;
+  const CustomText(
+    {
+      super.key,
+      required this.text,
+      this.isBold = false,
+      this.isItalic = false,
+      this.isCenter = false,
+      this.fontSize = TextUtil.textSize,
+      this.isOverFlow = false,
+      this.textColor,
+    }
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: isOverFlow ? TextOverflow.ellipsis : null,
+      style: TextStyle(
+        fontWeight: isBold ? FontWeight.bold : FontWeight.normal,
+        fontSize: fontSize,
+        fontStyle: isItalic ? FontStyle.italic : FontStyle.normal,
+        color: textColor ?? Theme.of(context).colorScheme.onPrimary
+      ),
+      textAlign: isCenter ? TextAlign.center : TextAlign.left,
+    );
+  }
+}
---
+++ lib/src/core/widget/utils/text_util.dart
@@ -0,0 +1,4 @@
+class TextUtil {
+  static const double textSize = 16.0;
+  static const double smallTextSize = 14.0;
+}
